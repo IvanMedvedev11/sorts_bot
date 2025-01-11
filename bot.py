55bb3997d780a5a70f60b0a3f97ba5e3893f2449
IvanMedvedev11 <medvivan11@gmail.com>
2025-01-11 17:29:35 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,8 @@
+from dotenv import load_dotenv
+from time import perf_counter
+from math import fsum
+from functions import *
+import os
 import telebot
 load_dotenv()
 TOKEN = os.getenv('TOKEN')
@@ -8,8 +13,18 @@ def start(message):
 @bot.message_handler(content_types=['text'])
 def sort(message):
     numbers = message.text.split(", ")
-    print(numbers)
-    numbers = list(map(float, numbers))
-    numbers = bubble_sort(numbers)
-    bot.send_message(message.chat.id, f"Отсортированный список: {numbers}")
+    try:
+        numbers = list(map(float, numbers))
+    except ValueError:
+        bot.send_message(message.chat.id, "Извините, но вы ввели не совсем верный список чисел(есть лишний символ), попробуйте еще раз")
+    else:
+        tic = perf_counter()
+        if fsum(numbers) / len(numbers) <= 1.0:
+            numbers = bubble_sort(numbers)
+        elif fsum(numbers) / len(numbers) <= 1000.0:
+            numbers = merge_sort(numbers)
+        else:
+            numbers = sorted(numbers)
+        toc = perf_counter()
+        bot.send_message(message.chat.id, f"Отсортированный список: {numbers}. \nПотраченное время: {toc - tic}")
 bot.infinity_polling()
